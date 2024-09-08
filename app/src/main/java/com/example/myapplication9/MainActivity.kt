8c7e37076657b5febd158e6a1020ddc834ece38e
soyeon <sykim0659@naver.com>
2024-09-08 22:20:16 +0900

    MediaPlayer로 음원재생

--- app/src/main/java/com/example/myapplication9/MainActivity.kt
+++ app/src/main/java/com/example/myapplication9/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.myapplication9
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication9.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var mediaPlayer : MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.playButton.setOnClickListener { mediaPlayerPlay() }
+        binding.pauseButton.setOnClickListener { mediaPlayerPause() }
+        binding.stopButton.setOnClickListener { mediaPlayerStop() }
+    }
+
+    private fun mediaPlayerPlay(){
+        if(mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.cheer).apply{
+                isLooping = true
+            }
         }
+        mediaPlayer?.start()
+    }
+    private fun mediaPlayerPause(){
+        mediaPlayer?.pause()
+    }
+    private fun mediaPlayerStop(){
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 }
